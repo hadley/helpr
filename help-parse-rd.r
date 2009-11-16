d79d522db69aa0b468e0104873b2e0209a46e20e
hadley <h.wickham@gmail.com>
2009-11-16 10:22:10 -0600

    Re-write of html converter to make it easier to understand

--- help-parse-rd.r
+++ help-parse-rd.r
@@ -1,3 +1,9 @@
+rd <- pkg_topic("stats", "glm")
+tags <- sapply(rd, tag)
+tags <- gsub("\\\\", "", tags)
+names(rd) <- tags
+
+
 tag <- function(x) attr(x, "Rd_tag")
 
 untag <- function(x) {
@@ -6,77 +12,96 @@ untag <- function(x) {
   x
 }
 
-reconstruct <- function(rd) {
-  if (is.null(rd)) return("")
+all_tags <- function(rd) {
+  if (is.null(rd)) return()
+  if (!is.list(rd)) return(tag(rd))
   
-  if (is.list(rd)) {
-    # Special tags get ignored
-    special <- tag(rd) == toupper(tag(rd))
-    
-    tag <- tolower(gsub("\\\\", "", tag(rd)))
-    
-    if (length(tag) >0 && tag == "link") {
-      fun <- rd[[1]]
-      pkg <- attr(rd, "Rd_option")
-      if (!is.null(pkg)) {
-        out <- str_join("<a href='/packages/", pkg, "/topics/", fun, "'>", fun, "</a>")        
-      } else {
-        out <- str_join("<a href='", fun, "'>", fun, "</a>")
-      }
-    } else if (tag == "url") {
-      out <- str_join("<a href='", rd[[1]], "'>", rd[[1]], "</a>")
-    } else {
-      html <- html_tags[tag]
-
-      prefix <- ifelse(special, "", sapply(html, "[", 1))
-      suffix <- ifelse(special, "", sapply(html, "[", 2))
-      out <- paste(
-        prefix, paste(sapply(rd, reconstruct), collapse = ""), suffix, 
-       sep = "")      
-    }
-    
-    if (tag(rd) == "TEXT") {
-      out <- str_split(out, "\\n\\n")[[1]]
-      out <- str_join("<p>", str_trim(out), "</p>", "\n\n", collapse = "")
-    } 
-    str_trim(out)
-  } else {
-    rd
-  }  
+  sort(unique(c(tag(rd), unlist(lapply(rd, all_tags)))))
 }
 
-html_tags <- list(
-  "acronym" = c('<acronym><span class = "acronym">','</span></acronym>'),
-  "bold" =         c("<b>", "</b>"),
-  "cite" =         c("<cite>", "</cite>"),
-  "code" =         c("<code>", "</code>"),
-  "command" =      c("<code>", "</code>"),
-  "dfn" =          c("<dfn>", "</dfn>"),
-  "donttest" =     c("", ""),
-  "dquote" =       c("&ldquo;", "&rdquo;"),
-  "emph" =         c("<em>", "</em>"),
-  "enumerate" =    c("<ol>", "</ol>"),
-  "env" =          c('<span class = "env">', '</span>'),
-  "file" =         c('&lsquo;<span class = "file">', '</span>&rsquo;'),
-  "link" =         c("<a>", "</a>"),
-  "item" =         c("<li>", ""),
-  "itemize" =      c("<ul>", "</ul>"),
-  "kbd" =          c("<kbd>", "</kbd>"),
-  "option" =       c('<span class = "option">',"</span>"),
-  "pkg" =          c('<span class = "pkg">',"</span>"),
-  "preformatted" = c("<pre>","</pre>"),
-  "samp" =         c('<span class = "samp">',"</span>"),
-  "squote" =       c("&lsquo;","&rsquo;"),
-  "strong" =       c("<strong>", "</strong>"),
-  "var" =          c("<var>", "</var>"),
-  "verb" =         c("<pre>", "</pre>"),
-  "text" =         c("<p>", "</p>"),
-  "R" =            c('<span style="R">R</span>', ""),
-  "cr" =           c("<br >", ""),
-  "dots" =         c("...", ""),
-  "ldots" =        c("...", "")
-)
+reconstruct <- function(rd) {
+  if (is.null(rd)) return()
+  
+  tag <- tag(rd)
+
+  if (length(tag) == 0 || tag %in% c("", "TEXT")) {
+    # Top level with no tag, just recurse
+    if (is.character(rd)) return(as.character(rd))
+
+    pieces <- sapply(rd, reconstruct)
+    text <- str_join(pieces, collapse = "")
+
+    if (length(tag) > 0 && tag == "") return(text)
+    
+    str_join("<p>", str_split(text, "\\n\\n")[[1]], "</p>", "\n\n", 
+      collapse = "")      
+
+  } else if (tag %in% names(simple_tags)) {
+    # If we can process tag with just prefix & suffix, do so
+    tag_simple(tag, reconstruct(untag(rd)))
+
+  } else if (tag == "\\link") {
+    stopifnot(length(rd) == 1)
+    fun <- rd[[1]]
+    pkg <- attr(rd, "Rd_option")
+    
+    tag_link(fun, pkg)
+
+  } else if (tag == "\\url") {
+    stopifnot(length(rd) == 1)
+    str_join("<a href='", rd[[1]], "'>", rd[[1]], "</a>")
+
+  } else {
+    message("Unknown tag ", tag)
+    reconstruct(untag(rd))
+  }
+}
+
+tag_simple <- function(tag, text) {
+  stopifnot(length(text) == 1)
+  html <- simple_tags[[tag]]
+  str_join(html[1], text, html[2])
+}
+
+tag_link <- function(fun, pkg = NULL) {
+  if (!is.null(pkg)) {
+    str_join("<a href='/packages/", pkg, "/topics/", fun, "'>", fun, "</a>")        
+  } else {
+    str_join("<a href='", fun, "'>", fun, "</a>")
+  }
+}
 
 
-escapes <- list(
+simple_tags <- list(
+  "\\acronym" =      c('<acronym>','</acronym>'),
+  "\\bold" =         c("<b>", "</b>"),
+  "\\cite" =         c("<cite>", "</cite>"),
+  "\\code" =         c("<code>", "</code>"),
+  "\\command" =      c("<code>", "</code>"),
+  "\\cr" =           c("<br >", ""),
+  "\\dfn" =          c("<dfn>", "</dfn>"),
+  "\\donttest" =     c("", ""),
+  "\\dots" =         c("...", ""),
+  "\\dquote" =       c("&ldquo;", "&rdquo;"),
+  "\\emph" =         c("<em>", "</em>"),
+  "\\enumerate" =    c("<ol>", "</ol>"),
+  "\\env" =          c('<span class = "env">', '</span>'),
+  "\\file" =         c('&lsquo;<span class = "file">', '</span>&rsquo;'),
+  "\\item" =         c("<li>", ""),
+  "\\itemize" =      c("<ul>", "</ul>"),
+  "\\kbd" =          c("<kbd>", "</kbd>"),
+  "\\ldots" =        c("...", ""),
+  "\\option" =       c('<span class = "option">',"</span>"),
+  "\\pkg" =          c('<span class = "pkg">',"</span>"),
+  "\\preformatted" = c("<pre>","</pre>"),
+  "\\R" =            c('<span style="R">R</span>', ""),
+  "\\samp" =         c('<span class = "samp">',"</span>"),
+  "\\squote" =       c("&lsquo;","&rsquo;"),
+  "\\strong" =       c("<strong>", "</strong>"),
+  "\\text" =         c("<p>", "</p>"),
+  "\\var" =          c("<var>", "</var>"),
+  "\\verb" =         c("<pre>", "</pre>"),
+  
+  "RCODE" =          c("", ""),
+  "VERB" =           c("", "")
 )
