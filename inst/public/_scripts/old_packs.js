d56b12f8a8c9aca55b0423eef5701d80b9993b45
Barret Schloerke <schloerke@gmail.com>
2010-05-25 17:17:27 -0500

    compromise on changes made

--- inst/public/_scripts/old_packs.js
+++ inst/public/_scripts/old_packs.js
@@ -1,9 +1,21 @@
+var showing_all = 0;
 function show_all_packages() {
   $("#packages").toggleClass("loaded");
+
+  showing_all = (showing_all + 1) %2;
+  if(showing_all == 0){    
+    $("#show_all_packs_button").attr("value", "Show All Packages");
+  }else{
+    $("#show_all_packs_button").attr("value", "Show Loaded Packages");    
+  }
+  
+  set_update_button();
 }
 
 // highlight all old, out-of-date packages
+var has_highlighted = 0;
 function highlight_old_packages() {
+  has_highlighted = 1;
   button = $("#out_of_date_button");
   button.attr("value", "Thinking...");
   button.attr("disabled", true)
@@ -16,15 +28,16 @@ function highlight_old_packages() {
         pkg = packages[i];
         $("#" + pkg).addClass("old");
       }
+      
+      set_update_button();
     }
   })
-  button.attr("value", "Updated");
   
 }
 
 // update all out-of-date packages
 function update_packs() {
-  var out_of_date_butto = document.getElementById("out_of_date_button");
+  var out_of_date_butto = $("#out_of_date_button");
   out_of_date_butto.value = "Updating...";
   out_of_date_butto.disabled = true;
 
@@ -36,6 +49,7 @@ function update_packs() {
         pkg = packages[i];
         $("#" + pkg).removeClass("old").addClass("update");
       }
+      set_update_button();
     }
   })
 }
@@ -51,7 +65,7 @@ function pluralize(count, word)
 function set_update_button(){
 
   // if highlight_old_packages has not been run, return 
-  if(has_exec_highlight == 0)
+  if(has_highlighted == 0)
     return;
 
   var i;
@@ -59,8 +73,13 @@ function set_update_button(){
   
   // count all visible rows that are out_of_date
   var packs = document.getElementById("packages").tBodies[0];
+  
+  // count all visible packages that are 'old'
   for(i = 0; i < packs.childNodes.length; i++){
-    if(packs.childNodes[i].nodeType==1 && hasClass(packs.childNodes[i], "out_of_date") && !hasClass(packs.childNodes[i], "hide")){
+    var pkg = packs.childNodes[i];
+
+    if(pkg.nodeType==1 && $(pkg).hasClass("old")  && ($(pkg).hasClass("loaded") || showing_all == 1)){
+      window.console.log( pkg + ": " + $(pkg).hasClass("old")  + "    :  "+ !$(pkg).hasClass("hide"));
       count++;
     }
   }
