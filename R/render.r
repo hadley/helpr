800ecad7d432649d07f16b3bbbd6b88a4fce0c8c
hadley <h.wickham@gmail.com>
2010-08-31 08:45:58 -0500

    Simplify renderers

--- R/render.r
+++ R/render.r
@@ -1,17 +1,3 @@
-# Render methods to eventually be moved into sinartra
-
-#' Render JSON
-#'
-#' Produce JSON from an R object
-#' @param object object to be turned into JSON
-#' @author Hadley Wickham and Barret Schloerke \email{schloerke@@gmail.com}
-#' @keywords internal 
-render_json <- function(object) {
-  json <- suppressWarnings(rjson::toJSON(object))
-  list(payload = json)
-}
-
-
 #' Render HTML Snippet
 #'
 #' @param template name of file to use in the folder "views"
@@ -20,18 +6,7 @@ render_json <- function(object) {
 #' @return text rendered from the template
 #' @author Barret Schloerke \email{schloerke@@gmail.com}
 #' @keywords internal 
-render_snippet <- function(template, params = list(), path = helpr_path){
-  if (is.list(params)) {
-      env <- new.env(TRUE)
-      for (name in names(params)) {
-          env[[name]] <- params[[name]]
-      }
-      params <- env
-  }
-  path <- file.path(path, "views", str_c("_",template, ".html"))
-  if (!file.exists(path)) 
-      stop("Can not find ", template, " template ", call. = FALSE)
-      
-  str_c(capture.output(brew:::brew(path, envir = params)), 
-        collapse = "\n")
+render_snippet <- function(template, params = list(), path = helpr_path) {
+  template <- str_c("_", template)
+  render_brew(template, params, path)$payload
 }
